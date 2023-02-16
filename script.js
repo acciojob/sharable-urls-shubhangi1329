f7c950547c93fb54dde4d1246c6362d4f79487cd
shubhangi1329 <117506539+shubhangi1329@users.noreply.github.com>
2023-02-16 16:46:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // your code here
+var name=querySelector('#name');
+var year=querySelector('#year');
+var btn=querySelector('#button');
+var form=querySelector('form');
+function shareurl(event){
+	var h3value=' https://localhost:8080/';
+	if(name.value && year.value){
+		h3value+='?name='+name.value+'&year='+year.value;  
+	}
+	else if(!name.value && year.value){
+		h3value+='?year='+year.value;
+	}
+	else if(!year.value && name.value){
+		h3value+='?name='+name.value;
+		
+	}
+	h3.textContent=h3value;
+}
+form.addEventListener('click',shareurl);
