eb92232f2246f6cdb8f814688a7a59332bfedbf3
shubhangi1329 <117506539+shubhangi1329@users.noreply.github.com>
2023-02-16 17:03:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ function shareurl(event){
 		h3value+='?name='+nameel+'&year='+yearel;  
 	}
 	else if(!nameel && yearel){
-		h3value+='?year='+yearel;
+		h3value+='?year='+ yearel;
 	}
 	else if(!yearel && nameel){
-		h3value+='?name='+nameel;
+		h3value+='?name='+ nameel;
 		
 	}
-	h3.textContent=h3value;
+	document.querySelector(h3).textContent=h3value;
 }
 form.addEventListener('submit',shareurl);
